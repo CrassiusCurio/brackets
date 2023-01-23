00a476aa9ddac79215e63a2aec1427255796e7be
CrassiusCurio <nuclearDrake@yandex.ru>
2023-01-23 23:55:50 +0400

    docs: Do you see how my mind works? Its like a laser!!!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const count = str.length / 2;
+  for (i = 0; i <= count; i++) {
+    bracketsConfig.forEach((el) => {
+      let regEx = new RegExp(`\\${el[0]}\\${el[1]}`, 'g');
+      str = str.replace(regEx, '');
+      let regExNum = new RegExp(`${el[0]}${el[1]}`, 'g');
+      str = str.replace(regExNum, '');
+    })
+
+  }
+  if (!str.length) { return true }
+  return false;
 }
